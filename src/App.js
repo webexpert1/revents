6474d8862cf2b0df4d5b38bf34f72bf1ce106be7
Richard Okoye <emmanuel.eze@netopng.com>
2020-01-03 10:18:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment} from 'react';
 import './App.css';
+import EventDashboard from './features/event/EventDashboard/EventDashboard';
+import NavBar from './features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+    </Fragment>
+   
+    
   );
 }
 
